e22eb43d922b898e23318e03d23afbeb25d6c721
Aliaksei Hrynko <aliaksei.hrynko@gmail.com>
2021-09-28 19:08:22 +0300

    Optimize the build

--- webpack.config.js
+++ webpack.config.js
@@ -22,7 +22,7 @@ const baseConfig = {
         test: /\.worker\.js$/,
         loader: 'worker-loader',
         options: {
-          inline: 'fallback',
+          inline: 'no-fallback',
         },
       },
     ],
