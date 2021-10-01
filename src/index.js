3434f2e1746cd53ffd25c45ae569132f2c165d91
Aliaksei Hrynko <aliaksei.hrynko@gmail.com>
2021-10-01 19:10:41 +0300

    Disable automatic installation

--- src/index.js
+++ src/index.js
@@ -1,32 +1,7 @@
-/* eslint-disable no-undef */
-
 import VuePdfEmbed from './vue-pdf-embed.vue'
 
-function install(Vue) {
-  if (install.installed) {
-    return
-  }
-
-  install.installed = true
-  Vue.component('VuePdfEmbed', VuePdfEmbed)
+if (typeof window !== 'undefined' && window.Vue) {
+  window.VuePdfEmbed = VuePdfEmbed
 }
-
-const plugin = {
-  install,
-}
-
-let GlobalVue = null
-
-if (typeof window !== 'undefined') {
-  GlobalVue = window.Vue
-} else if (typeof global !== 'undefined') {
-  GlobalVue = global.vue
-}
-
-if (GlobalVue) {
-  GlobalVue.use(plugin)
-}
-
-VuePdfEmbed.install = install
 
 export default VuePdfEmbed
