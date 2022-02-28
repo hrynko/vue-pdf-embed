1e60e71473d670f7addfd5b56daed87383df2241
Aliaksei Hrynko <aliaksei.hrynko@gmail.com>
2022-02-28 09:08:13 +0300

    Update demo styles

--- demo/App.vue
+++ demo/App.vue
@@ -34,16 +34,19 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
 body {
   padding: 16px;
   background-color: #ccc;
 }
 
-canvas {
-  display: block;
+.vue-pdf-embed {
   margin: auto;
   max-width: 480px;
-  box-shadow: 0 2px 8px 4px rgba(0, 0, 0, 0.1);
+
+  & > div {
+    margin-bottom: 4px;
+    box-shadow: 0 2px 8px 4px rgba(0, 0, 0, 0.1);
+  }
 }
 </style>
