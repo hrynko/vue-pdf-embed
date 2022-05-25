8d059429eb68ba42384f24ecc6e189274449f53d
Aliaksei Hrynko <aliaksei.hrynko@gmail.com>
2022-05-25 22:55:06 +0300

    Attach getDocument to module

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import VuePdfEmbed from './vue-pdf-embed.vue'
+import { getDocument } from 'pdfjs-dist/legacy/build/pdf.js'
+
+VuePdfEmbed.getDocument = getDocument
 
 if (typeof window !== 'undefined' && window.Vue) {
   window.VuePdfEmbed = VuePdfEmbed
